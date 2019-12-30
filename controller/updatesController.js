c4381b216957de527dcd3c219794d7634dd841db
ICruz9 <ignacio1999montero@hotmail.com>
2019-12-30 16:55:54 -0600

    Nuevo endpoint y procedure (updateUser)

--- controller/updatesController.js
+++ controller/updatesController.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var eventManager = require('../manager/updatesManager');
-
+router.get('/updateUser', function(req, res, next) { //post
+    try {
+        eventManager.updateUser(req.body).then(
+            (data) => {
+                let response = {
+                  content: data.recordset,
+                  success: data.output,
+                  code: 200
+                };
+                res.send(JSON.stringify(response));
+            }
+        );
+    }
+    catch (err) {
+        let response = {
+          content: err,
+          code: 500
+        };
+        res.send(JSON.stringify(response));
+    }
+  });
 module.exports = router;
--- manager/updatesManager.js
+++ manager/updatesManager.js
@@ -1,2 +1,28 @@
 var sql = require('mssql');
 var poolConnection = require('../conection/connection');
+exports.updateUser = async (req) => {
+    try {
+        console.log(req)
+        const pool = await poolConnection.getConnection();
+        let result = await pool.request()
+            .input('id_usuario',            sql.Int,    1003)
+            .input('name',              sql.VarChar(75),   'Cristian Nodal')
+            .input('email',               sql.VarChar(50),    'otro@gmail.com')
+            .input('rol',                sql.VarChar(15),    'Miembro')
+            //.input('nameuser',                sql.VarChar(10),    'crnodal')
+            //.input('password',                sql.VarChar(10),    '12345')
+            .input('number',                      sql.VarChar(8),   '88339374')
+            .output('Resultado',                sql.Bit)
+            .output('Resultado',    sql.Bit)
+            .execute('editUser');
+            
+        sql.close();
+        console.log(result);
+        return result;
+
+    } catch (err) {
+        sql.close();
+        console.log(err);
+        return "Error en " + err;
+    }
+};
